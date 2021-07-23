3515e210de1da2cfa6d9d224a44b573c0e25fd0b
La-wii <sentilavale@gmail.com>
2021-07-23 14:15:11 +0200

    init

---
+++ doc.php
@@ -0,0 +1,41 @@
+<?php
+$titolo="Il Lonfo";
+?>
+
+
+
+<?php
+$paragrafo="Il Lonfo non vaterca né gluisce
+e molto raramente barigatta,
+ma quando soffia il bego a bisce bisce,
+sdilenca un poco e gnagio s’archipatta.
+È frusco il Lonfo! È pieno di lupigna
+arrafferia malversa e sofolenta!
+Se cionfi ti sbiduglia e ti arrupigna
+se lugri ti botalla e ti criventa.
+Eppure il vecchio Lonfo ammargelluto
+che bete e zugghia e fonca nei trombazzi
+fa legica busia, fa gisbuto;
+e quasi quasi in segno di sberdazzi
+gli affarferesti un gniffo. Ma lui, zuto
+t’alloppa, ti sbernecchia;
+e tu l’accazzi.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio PHP</title>
+</head>
+<body>
+    <h1>
+        <?php echo $titolo ?>
+    </h1>
+    <p>
+        <?php echo $paragrafo ?>
+    </p>
+</body>
+</html>
